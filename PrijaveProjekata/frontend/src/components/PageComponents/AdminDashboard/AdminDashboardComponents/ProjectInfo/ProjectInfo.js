91c04dd1b62d02641d5870b6570084b65fb3055a
culjakluka <lculja00@fesb.hr>
2024-05-03 12:51:11 +0200

    merge conflict

--- PrijaveProjekata/frontend/src/components/PageComponents/AdminDashboard/AdminDashboardComponents/ProjectInfo/ProjectInfo.js
+++ PrijaveProjekata/frontend/src/components/PageComponents/AdminDashboard/AdminDashboardComponents/ProjectInfo/ProjectInfo.js
@@ -15,6 +15,7 @@ import PdfDownload from "../../../../test/PdfDownload.jsx";
 import CsvConverter from "../../../../test/csv/CsvConverter.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { set } from "mongoose";
 
 const ProjectInfo = ({ selectedProject }) => {
   const [selectedProjectData, setSelectedProjectData] = useState();
@@ -23,9 +24,9 @@ const ProjectInfo = ({ selectedProject }) => {
     AdminDashboardContext
   );
 
-  useEffect(() => {}, [selectedProject]);
-
-  useEffect(() => {}, [selectedProject]);
+  useEffect(() => {
+    setSelectedProjectData(selectedProject);
+  }, []);
 
   // DATA
 
