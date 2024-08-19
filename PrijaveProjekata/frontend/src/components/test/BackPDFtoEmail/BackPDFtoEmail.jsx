f894d0c057d231922ba64add0c3a4b231ccdf181
Luka Culjak <llukaculjak@gmail.com>
2024-08-19 18:42:02 +0200

    changed the mock data on backend pdf generation to test new work positions flattener

--- PrijaveProjekata/frontend/src/components/test/BackPDFtoEmail/BackPDFtoEmail.jsx
+++ PrijaveProjekata/frontend/src/components/test/BackPDFtoEmail/BackPDFtoEmail.jsx
@@ -7,82 +7,109 @@ class BackPDFtoEmail extends React.Component {
       isLoading: false,
     };
   }
-
+  
   generatePDF = async () => {
     try {
       const projectData = {
-        _id: "663bac755e9c439885b0e99a",
-        applicationDeadline: "2024-06-11T00:00:00.000Z",
+        _id: "66ba3745740a41bf28d34d95",
+        userId: "65a56e9b54fb67b1b39e706c",
+        firstInputMarker: true,
+        secondInputMarker: true,
+        nameSurname: "Perica Peric Perislav",
+        vocation: "Doktor",
+        department: "Zavod za strojarstvo i brodogradnju - dr.sc. Damir Sedlar",
+        email: "perica@gmail.com",
+        projectTitle: "PrijaveProjekata 2",
+        projectAcronym: "PrijProje",
+        applicationDeadline: 1724882400000,
+        projectSummary: "Ovo je kratak sazetak projekta",
         applicationURL: "www.fesb.hr",
+        projectApplicant: "FESB",
+        projectPartners: "Nema 1ostalih partnera",
+        totalValue: 100000,
+        fesbValuePart: 20000,
+        newEmploymentBoolean: true,
+        newEmploymentPositions: [
+          {
+            positionName: "sdsd",
+            positionSalary: 323,
+            positionPercentage: 23,
+            _id: "66ba3745740a41bf28d34d96"
+          },
+          {
+            positionName: "Tajnik",
+            positionSalary: 800,
+            positionPercentage: 23,
+            _id: "66ba3745740a41bf28d34d97"
+          },
+          {
+            positionName: "1",
+            positionSalary: 1,
+            positionPercentage: 1,
+            _id: "66c1de9be3e02eda670ad87e"
+          }
+        ],
+        projectTeam: [
+          {
+            nameSurname: "Mali Perica",
+            email: "mail@gmail.com",
+            thisProjectPercentage: 80,
+            otherProjects: [
+              {
+                otherProjectName: "Projekt2",
+                otherProjectPercentage: 30,
+                _id: "66ba3745740a41bf28d34d99"
+              }
+            ],
+            _id: "66ba3745740a41bf28d34d98"
+          }
+        ],
+        pdfDocuments: {
+          pdfs: []
+        },
+        state: "secondFormSubmitted",
+        createdAt: 1723479877071,
+        updatedAt: 1723981467366,
+        __v: 0,
         consultantExpense: 1,
         consultantExpenseSource: "1",
         consultantServices: true,
-        createdAt: "2024-05-08T16:46:45.581Z",
-        currentPesonnelExpense: 12,
-        department: "Zavod za elektroenergetiku",
-        downPayment: 0,
+        currentPersonnelExpense: 1,
+        downPayment: 1,
         economicSubjectInvolvement: true,
-        email: "miroljub.petrovic@gmail.com",
-        equipmentAmortizationExpense: 23,
-        equipmentDescriptionAndExpense: "22",
-        expectedProjectBeginning: "2000-12-31T23:00:00.000Z",
+        equipmentAmortizationExpense: 1,
+        equipmentDescriptionAndExpense: "1",
+        expectedProjectBeginning: 1724968800000,
         expectedProjectDurationInMonths: 1,
-        expenseNote: "Nema napomene",
-        fesbValuePart: 10073,
-        firstInputMarker: true,
-        materialExpense: 123,
-        mobilePhoneNumber: 981399662,
-        nameSurname: "Miroljub Petrović",
-        newEmploymentBoolean: true,
-        newPersonnelExpense: 23,
-        otherServicesExpense: 123,
-        partnerExpense: 31,
-        personalFinancingExpense: "Nema",
-        projectAcronym: "PoMaBa",
-        projectApplicant: "Miroljub Petrović",
-        projectPartners: "Nema ostalih partnera",
-        projectSummary: "Ovo je dug sadrzaj",
-        projectTeam: [
-          {
-            email: "la",
-            nameSurname: "Miroljub Petrović",
-            thisProjectPercentage: 10,
-            otherProjects: [
-              {
-                otherProjectName: "Pod Mač Bato",
-                otherProjectPercentage: 10,
-              },
-            ],
-          },
-        ],
-        projectTitle: "Pod Mač Bato",
-        projectType: "Međunarodni znanstveni kompetitivni",
-        requestedFunding: 0,
-        requiredDocumentationFESB: "213213",
-        secondInputMarker: false,
-        sourceOfFunding: "Nema",
-        state: "firstFormSubmitted",
-        teamLeaderNote: "Nema napomene",
-        totalValue: 213,
-        travelRegistrationEducationExpense: 21321,
-        updatedAt: "2024-05-08T22:29:28.191Z",
-        userId: "65a56e9b54fb67b1b39e706c",
-        vocation: "Dr.",
-        workTimeOtherPercentage: 10,
-        workTimeThisPercentage: 10,
+        expenseNote: "1",
+        indirectExpenses: 3000,
+        materialExpense: 1,
+        mobilePhoneNumber: "1",
+        newPersonnelExpense: 1,
+        otherServicesExpense: 1,
+        partnerExpense: 1,
+        personalFinancingExpense: "1",
+        projectType: "Infrastrukturni projekt",
+        requestedFunding: 1,
+        requiredDocumentationFESB: "1",
+        sourceOfFunding: "Horizon Europe",
+        teamLeaderNote: "1",
+        travelRegistrationEducationExpense: 1,
+        workTimeOtherPercentage: 1,
+        workTimeThisPercentage: 1
       };
-      this.setState({ isLoading: true });
+this.setState({ isLoading: true });
 
-      // Make a request to the server to generate the PDF
-      const response = await fetch("/api/generate-pdf", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ projectData: projectData }),
-      });
+// Make a request to the server to generate the PDF
+const response = await fetch("/api/generate-pdf", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ projectData: projectData }),
+});
 
-      if (!response.ok) {
+if (!response.ok) {
         throw new Error("Failed to generate PDF");
       }
 
